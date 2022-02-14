63c4786d5de6eb5af862f56e3c84bbda51836d5c
Jose Quintero <jolquinteroc@correo.udistrital.edu.co>
2022-02-14 18:47:48 -0500

    Back de ventas

--- componentes/core/edu/core/nucleo.py
+++ componentes/core/edu/core/nucleo.py
@@ -15,3 +15,11 @@ class Producto(IRegla):
         tk.Message(master, text="Eliminar producto").pack()
         tk.Message(master, text="Leyendo producto").pack()
         tk.Message(master, text="Actualizando producto").pack()
+        tk.mainloop()                
+        
+class Ventas(IRegla):
+    def verificar_reglas(self):        
+        master = tk.Tk()       
+        tk.Message(master, text="Crear venta").pack()
+        tk.Message(master, text="Obtener ventas").pack()
+        tk.mainloop() 
